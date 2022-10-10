916320ea2d817bd814fe61fd3b7deb0eea168012
Rustem <r.zhumagulov@gmail.com>
2022-10-10 11:40:01 +0600

    Comments for monitor object change demo

--- src/sync/IncorrectSynchronizationDemo.java
+++ src/sync/IncorrectSynchronizationDemo.java
@@ -9,34 +9,28 @@ class IncorrectSynchronizationDemo {
 
 class IncorrectSynchronization {
 
-    Boolean flag = true;
+    Boolean flag = true; // should be final
 
     public void example() throws InterruptedException {
 
-        Thread t1 = new Thread(new Runnable() {
-
-            public void run() {
-                synchronized (flag) {
-                    try {
-                        while (flag) {
-                            System.out.println("First thread about to sleep");
-                            Thread.sleep(5000);
-                            System.out.println("Woke up and about to invoke wait()");
-                            flag.wait();
-                        }
-                    } catch (InterruptedException ie) {
-
+        Thread t1 = new Thread(() -> {
+            synchronized (flag) {
+                try {
+                    while (flag) {
+                        System.out.println("First thread about to sleep");
+                        Thread.sleep(5000);
+                        System.out.println("Woke up and about to invoke wait()");
+                        flag.wait(); // Will throw IllegalMonitorException cause monitor object have been changed by other thread
                     }
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
                 }
             }
         });
 
-        Thread t2 = new Thread(new Runnable() {
-
-            public void run() {
-                flag = false;
-                System.out.println("Boolean assignment done.");
-            }
+        Thread t2 = new Thread(() -> {
+            flag = false; // Changing monitor object is huge mistake.
+            System.out.println("Boolean assignment done.");
         });
 
         t1.start();
