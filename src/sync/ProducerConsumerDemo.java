0c3abb7e62124dddeade23d2a845fe293fec09af
Rustem <r.zhumagulov@gmail.com>
2022-10-17 12:24:39 +0600

    Producer-consumer v2

--- src/sync/ProducerConsumerDemo.java
+++ src/sync/ProducerConsumerDemo.java
@@ -1,5 +1,9 @@
 package sync;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ProducerConsumerDemo {
 
     public static void main(String[] args) throws InterruptedException {
@@ -32,22 +36,58 @@ public class ProducerConsumerDemo {
 
 class Process {
 
+    private final List<Integer> integers = new ArrayList<Integer>();
+    private final int max = 5;
+    private final int min = 0;
+    private final Object lock = new Object();
+    private final Random random = new Random(1);
+
     public void produce() throws InterruptedException {
-        synchronized (this) {
-            System.out.println("Producing process");
-            wait();
-            System.out.println("Resuming producing process");
+        synchronized (lock) {
+
+            while (true) {
+
+                if (integers.size() == max) {
+
+                    System.out.println("Waiting to consumer free up the list");
+
+                    lock.wait();
+                } else {
+
+                    int randomInt = random.nextInt();
+
+                    integers.add(randomInt);
+
+                    System.out.println("Produced value: " + randomInt);
+
+                    lock.notify();
+                }
+            }
         }
     }
 
     public void consume() throws InterruptedException {
-        synchronized (this) {
-            System.out.println("Consuming process");
-            notify();
-            System.out.println("Ending consumer process"); //the code will be executed anyway
-            Thread.sleep(5000L);
-            System.out.println("Consumer process have ended");
-            //only after execution, lock will be released
+        synchronized (lock) {
+
+            while (true) {
+
+                if (integers.size() == min) {
+
+                    System.out.println("Waiting to producer");
+
+                    lock.wait();
+
+                } else {
+
+                    System.out.println("Removing: " + integers.remove(integers.size() - 1));
+
+                    lock.notify();
+                }
+
+//                if (integers.size() == min) lock.notify();
+
+//                System.out.println("Consumer process have ended");
+            }
         }
     }
 }
